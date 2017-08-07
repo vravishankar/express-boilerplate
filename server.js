47a39f79d170cff5ce6a9b5dc5935cdef5760f6a
Ravi Shankar <ravi_shankar_v@yahoo.com>
2017-08-07 20:44:13 +0800

    Updated server.js

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
 const db = require('./config/db');
 
 const app = express();
-
-var mongodb;
 
 //Middlewares
 app.use(bodyParser.json())
@@ -16,14 +13,9 @@ const port = 8000;
 
 MongoClient.connect(db.url, (err, db) => {
 
-    assert.equal(null, err);
-    //require('./app/routes')(app, db)
-    mongodb = db;
+    require('./app/routes')(app, db)
 
     app.listen(port, () => {
         console.log(`Server Listening on Port ${port}`);
     });
 })
-
-
-console.log(mongodb);
